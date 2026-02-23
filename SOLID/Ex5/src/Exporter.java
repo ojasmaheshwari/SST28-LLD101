7ef6b027fcfd3a2e9991548ddb7a0e0e3f0856d1
Ojas Maheshwari <ojasmaheshwari2006@gmail.com>
2026-02-23 11:10:12 +0530

    Completed Ex5

--- SOLID/Ex5/src/Exporter.java
+++ SOLID/Ex5/src/Exporter.java
@@ -1,4 +1,22 @@
 public abstract class Exporter {
-    // implied "contract" but not enforced (smell)
-    public abstract ExportResult export(ExportRequest req);
+
+	// Base contract
+	protected void validate(ExportRequest req) {
+		if (req == null) {
+			throw new IllegalArgumentException("request cannot be null");
+		}
+		if (req.title == null) {
+			throw new IllegalArgumentException("title cannot be null");
+		}
+		if (req.body == null) {
+			throw new IllegalArgumentException("body cannot be null");
+		}
+	}
+
+	public ExportResult export(ExportRequest req) {
+		validate(req);
+		return doExport(req);
+	}
+
+	protected abstract ExportResult doExport(ExportRequest req);
 }
--- SOLID/Ex5/src/PdfExporter.java
+++ SOLID/Ex5/src/PdfExporter.java
@@ -1,13 +1,25 @@
 import java.nio.charset.StandardCharsets;
 
 public class PdfExporter extends Exporter {
-    @Override
-    public ExportResult export(ExportRequest req) {
-        // LSP violation: tightens precondition arbitrarily
-        if (req.body != null && req.body.length() > 20) {
-            throw new IllegalArgumentException("PDF cannot handle content > 20 chars");
-        }
-        String fakePdf = "PDF(" + req.title + "):" + req.body;
-        return new ExportResult("application/pdf", fakePdf.getBytes(StandardCharsets.UTF_8));
-    }
+	@Override
+	public void validate(ExportRequest req) {
+		if (req == null) {
+			throw new IllegalArgumentException("request cannot be null");
+		}
+		if (req.title == null) {
+			throw new IllegalArgumentException("title cannot be null");
+		}
+		if (req.body == null) {
+			throw new IllegalArgumentException("body cannot be null");
+		}
+		if (req.body.length() > 20) {
+			throw new IllegalArgumentException("PDF cannot handle content > 20 chars");
+		}
+	}
+
+	@Override
+	public ExportResult doExport(ExportRequest req) {
+		String fakePdf = "PDF(" + req.title + "):" + req.body;
+		return new ExportResult("application/pdf", fakePdf.getBytes(StandardCharsets.UTF_8));
+	}
 }
